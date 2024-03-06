a1f67f6003f3097e60ae3a697c9bbf53aaaf749e
Avaz <elonarm21@gmail.com>
2024-03-06 16:34:29 +0500

    converted to class components and fixed counter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,42 +1,48 @@
+import React from "react";
 import Card from "./Card.jsx";
 
-const App = () => {
-    const data = [
-        {
-            id: 1,
-            title: "NextJS",
-            desc: "Full-Stack",
-            src: "https://sammi.ac/_next/image?url=https%3A%2F%2Futfs.io%2Ff%2F70e1537d-4afc-4cfe-8915-58a814e623da-kilw5u.png&w=1920&q=75"
-        },
-        {
-            id: 2,
-            title: "ReactJS",
-            desc: "Front-End",
-            src: "https://sammi.ac/_next/image?url=https%3A%2F%2Futfs.io%2Ff%2Fa8573b7c-95b2-4459-8414-8eacde874b0a-kilwdl.png&w=1920&q=75"
-        },
-        {
-            id: 3,
-            title: "JavaScript",
-            desc: "Front-End",
-            src: "https://sammi.ac/_next/image?url=https%3A%2F%2Futfs.io%2Ff%2F35ca3db9-fb43-4f12-bd48-8b08a503db09-kilwwj.png&w=1920&q=75"
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {
+                    id: 1,
+                    title: "NextJS",
+                    desc: "Full-Stack",
+                    src: "https://sammi.ac/_next/image?url=https%3A%2F%2Futfs.io%2Ff%2F70e1537d-4afc-4cfe-8915-58a814e623da-kilw5u.png&w=1920&q=75"
+                },
+                {
+                    id: 2,
+                    title: "ReactJS",
+                    desc: "Front-End",
+                    src: "https://sammi.ac/_next/image?url=https%3A%2F%2Futfs.io%2Ff%2Fa8573b7c-95b2-4459-8414-8eacde874b0a-kilwdl.png&w=1920&q=75"
+                },
+                {
+                    id: 3,
+                    title: "JavaScript",
+                    desc: "Front-End",
+                    src: "https://sammi.ac/_next/image?url=https%3A%2F%2Futfs.io%2Ff%2F35ca3db9-fb43-4f12-bd48-8b08a503db09-kilwwj.png&w=1920&q=75"
+                }
+            ]
         }
-    ]
-    return (
-        <div className={"app"}>
+    }
+
+    render() {
+        const {data} = this.state
+        return (<div className={"app"}>
             <div className="container">
                 <div className="row">
-                    {data.map(({id, title, desc, src}) => (
-                        <Card
-                            title={title}
-                            desc={desc}
-                            src={src}
-                            key={id}
-                        />)
-                    )}
+                    {data.map(({id, title, desc, src}) => (<Card
+                        title={title}
+                        desc={desc}
+                        src={src}
+                        key={id}
+                    />))}
                 </div>
             </div>
-        </div>
-    )
+        </div>)
+    }
 }
 
 export default App
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,29 @@
 // eslint-disable-next-line react/prop-types
-const Card = ({title, desc, src}) => {
-    return (
-        <div className="col-4 my-5">
+import React from "react";
+import {Button} from "@mui/material";
+
+// eslint-disable-next-line react/prop-types
+class Card extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0
+        }
+    }
+
+    onIncrease = () => {
+        this.setState(prevState => ({counter: prevState.counter + 1}))
+    }
+
+    onDecrease = () => {
+        this.setState(prevState => ({counter: prevState.counter - 1}))
+    }
+
+    render() {
+        // eslint-disable-next-line react/prop-types
+        const {title, desc, src} = this.props
+        const {counter} = this.state
+        return (<div className="col-4 my-5">
             <div className="card">
                 <div className="card-header">
                     <h4 className={"m-0"}>{title}</h4>
@@ -12,14 +34,14 @@ const Card = ({title, desc, src}) => {
                 </div>
                 <div className="card-footer d-flex justify-content-center">
                     <div className="btn-group">
-                        <button className="btn btn-sm btn-danger fs-5">-</button>
-                        <button disabled={true} className={"btn btn-sm btn-secondary fs-5"}>0</button>
-                        <button className="btn btn-sm btn-success fs-5">+</button>
+                        <Button onClick={this.onIncrease}>-</Button>
+                        <Button disabled={true}>{counter}</Button>
+                        <Button onClick={this.onDecrease}>+</Button>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        </div>)
+    }
 }
 
 export default Card
